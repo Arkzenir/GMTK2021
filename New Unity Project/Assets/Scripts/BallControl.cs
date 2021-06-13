9f15001785b6d1419171ff9a75dd1a2d629422ba
Alper Sari <alper.sari.work@gmail.com>
2021-06-13 19:16:41 +0300

    Added entry dialogue to Area 2, added movement feeature to ball control

--- New Unity Project/Assets/Scripts/BallControl.cs
+++ New Unity Project/Assets/Scripts/BallControl.cs
@@ -6,7 +6,13 @@ public class BallControl : MonoBehaviour
     public bool spin = false;
     public bool release = false;
     public Animator animator;
-    
+
+    private CircleCollider2D circle;
+
+    private void Start()
+    {
+        circle = gameObject.GetComponent<CircleCollider2D>();
+    }
 
 
     // Update is called once per frame
@@ -18,6 +24,7 @@ public class BallControl : MonoBehaviour
                 && spin == false)
             {
                 spin = true;
+                circle.isTrigger = false;
             }
         
         }
@@ -26,7 +33,7 @@ public class BallControl : MonoBehaviour
     
     
 
-    void FixedUpdate()
+    private  void FixedUpdate()
     {
         if (!Input.GetButton("Fire1"))
         {
@@ -40,6 +47,7 @@ public class BallControl : MonoBehaviour
         if (Input.GetMouseButton(1)
         && spin)
         {
+            circle.isTrigger = true;
             spin = false;
             release = true;
 
